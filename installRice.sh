511fd08a4935d3f9c5f9854dfd2799365c30dde9
Awnrt <roman.levikoff@gmail.com>
2021-10-24 01:30:16 +0000

    Uploaded

--- installRice.sh
+++ installRice.sh
@@ -18,3 +18,7 @@ echo "exec dwm" >> /home/jimbob/.xinitrc
 echo "PS1='\[\e[0;38;5;203m\][\[\e[0;38;5;214m\]\u\[\e[0;38;5;34m\]@\[\e[0;38;5;111m\]\h\[\e[0m\] \[\e[0;38;5;147m\]\W\[\e[0;38;5;203m\]]\[\e[0m\]$\[\e[0m\] \[\e[0m\]'" >> /home/jimbob/.bashrc
 sudo pacman -S alsa-utils pulseaudio pulsemixer
 sudo localectl --no-convert set-x11-keymap us,ru "" "" grp:ctrl_shift_toggle
+sudo mv /usr/share/icons/default/index.theme backupindex.theme
+sudo cp /home/jimbob/arch/macOSBigSur/index.theme /usr/share/icons/default
+sudo cp /home/jimbob/arch/macOSBigSur/cursor.theme /usr/share/icons/default
+sudo cp -r /home/jimbob/arch/macOSBigSur/cursors /usr/share/icons/default
