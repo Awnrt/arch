b15067c381bb39ec386905ac7adba47f60a8fa84
Awnrt <74102659+Awnrt@users.noreply.github.com>
2021-10-29 02:28:37 +0300

    Update installKDE.sh

--- installKDE.sh
+++ installKDE.sh
@@ -1,6 +1,7 @@
-sudo pacman -S networkmanager
+sudo pacman -Sy networkmanager
 sudo systemctl enable NetworkManager.service
 sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twn xorg-xclock xterm
 sudo pacman -S plasma sddm
 sudo systemctl enable sddm.service
+sudo pacman -S konsole
 reboot
