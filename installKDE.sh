2352c7108141777da88cf81570db754b75a788ff
Awnrt <74102659+Awnrt@users.noreply.github.com>
2021-11-17 01:54:56 +0000

    Delete installKDE.sh

--- installKDE.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -Sy networkmanager
-sudo systemctl enable NetworkManager.service
-sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twn xorg-xclock xterm
-sudo pacman -S plasma sddm
-sudo systemctl enable sddm.service
-sudo pacman -S konsole
-reboot
