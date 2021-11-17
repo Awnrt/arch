615905c9e0f936c9ac307a43ffb8be748d6c9bde
Awnrt <roman.levikoff@gmail.com>
2021-11-17 04:12:04 +0000

    Updated

--- post_chroot.sh
+++ post_chroot.sh
@@ -1,47 +1,92 @@
-#Locale installation
+LIGHTGREEN='\033[1;32m'
+LIGHTRED='\033[1;91m'
+WHITE='\033[1;97m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;96m'
+NoColor='\033[0m'
+
+printf ${MAGENTA}"Generating locale...\n"
+printf ${LIGHTGREEN}""
+
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
-#Setting timezone
+printf ${MAGENTA}"Setting up timezone...\n"
+printf ${LIGHTGREEN}""
+
 ln -s /usr/share/zoneinfo/Europe/Moscow > /etc/localtime
 hwclock --systohc --utc
-#Setting hostname
-echo overlord > /etc/hostname
-#Enabling multilib
-echo "Enabling multilib..."
+clear
+printf ${MAGENTA}"Type in hostname: "
+printf ${NoColor}""
+read -p "" _hostname
+_hostname=${_hostname:-overlord}
+
+echo $_hostname > /etc/hostname
+
+printf ${MAGENTA}"Enabling multilib...\n"
+printf ${LIGHTGREEN}""
+
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Sy
-#For SSDs
+
+printf ${MAGENTA}"Enabling fstrim.timer...\n"
+printf ${LIGHTGREEN}""
 systemctl enable fstrim.timer
-#Useradd
-useradd -m -g users -G wheel,storage,power -s /bin/bash jimbob
-#Setting password for root and user
+
+printf ${MAGENTA}"Username: "
+printf ${NoColor}""
+read -p "" _username
+_username=${_username:-jimbob}
+
+useradd -m -g users -G wheel,storage,power -s /bin/bash $_username
+
 echo "Enter root password"
 passwd
 echo "Enter user password"
-passwd jimbob
-#Sudoers adding wheel group to users
+passwd $_username
+
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 echo "Defaults rootpw" >> /etc/sudoers
-#Installing bootloader
+
+printf ${MAGENTA}"Installing bootloader...\n"
+printf ${LIGHTGREEN}""
+
 bootctl install
-#Settings boot options
+
+printf ${MAGENTA}"Installing intel-ucode...\n"
+printf ${LIGHTGREEN}""
+
 pacman -S intel-ucode
+
+printf ${MAGENTA}"Creating configuration file for bootloader...\n"
+printf ${LIGHTGREEN}""
+
 echo "title Arch Linux" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-#Hardening boot path
-echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/nvme1n1p3) rw" nvidia-drm.modeset=1 intel_iommu=on >> /boot/loader/entries/arch.conf
-#DHCPCD installation
+
+echo "options root=PARTUUID=$(blkid -s PARTUUID -o value /dev/$disk_drive"3") rw" nvidia-drm.modeset=1 intel_iommu=on >> /boot/loader/entries/arch.conf
+
+clear
+printf ${MAGENTA}"Installing DHCPCD...\n"
+printf ${LIGHTGREEN}""
+
 pacman -S dhcpcd
 systemctl enable dhcpcd@eno1.service
-#Installing NVIDIA Drivers
+
+printf ${MAGENTA}"Installing NVIDIA Drivers...\n"
+printf ${LIGHTGREEN}""
+
 pacman -S linux-headers
 pacman -S nvidia-dkms nvidia-utils opencl-nvidia libglvnd lib32-libglvnd lib32-nvidia-utils lib32-opencl-nvidia nvidia-settings
-#Creating hooks for NVIDIA
+
+printf ${MAGENTA}"Creating hooks for NVIDIA...\n"
+printf ${LIGHTGREEN}""
+
 mkdir /etc/pacman.d/hooks
 echo "[Trigger]" >> /etc/pacman.d/hooks/nvidia
 echo "Operation=Install" >> /etc/pacman.d/hooks/nvidia
@@ -53,7 +98,6 @@ echo "[Action]" >> /etc/pacman.d/hooks/nvidia
 echo "Depends=mkinitcpio" >> /etc/pacman.d/hooks/nvidia
 echo "When=PostTransaction" >> /etc/pacman.d/hooks/nvidia
 echo "Exec=/usr/bin/mkinitcpio -P" >> /etc/pacman.d/hooks/nvidia
-#Modules
-echo "Now you need to add this stuff into modules list PATH /etc/mkinitcpio.conf"
-echo "MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)"
-echo "And after that you can EXIT and reboot your system"
+
+printf ${LIGHTRED}"\nFinished!\nNow you need to add this stuff into modules list PATH /etc/mkinitcpio.conf\nMODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)\nAnd after that you can EXIT and reboot your system\n"
+printf ${LIGHTGREEN}""
