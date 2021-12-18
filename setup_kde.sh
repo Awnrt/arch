413fd55b245bfedf334536a28d8783e4645fc86a
Awnrt <74102659+Awnrt@users.noreply.github.com>
2021-12-18 14:09:34 +0300

    Update setup_kde.sh

--- setup_kde.sh
+++ setup_kde.sh
@@ -8,12 +8,12 @@ NoColor='\033[0m'
 clear
 printf ${MAGENTA}"Installing KDE...\n"
 printf ${LIGHTGREEN}""
-pacman -Sy networkmanager
+pacman -Sy networkmanager --noconfirm
 systemctl enable NetworkManager.service
-pacman -S xorg-server xorg-apps xorg-xinit xorg-twn xorg-xclock xterm
-pacman -S plasma sddm
+pacman -S xorg-server xorg-apps xorg-xinit xorg-twn xorg-xclock xterm --noconfirm
+pacman -S plasma sddm --noconfirm
 systemctl enable sddm.service
-pacman -S konsole
+pacman -S konsole --noconfirm
 
 printf ${LIGHTRED}"\nFinished!\nNow you need to add this stuff into modules list PATH /etc/mkinitcpio.conf\nMODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)\nAnd after that you can EXIT and reboot your system\n"
 printf ${LIGHTGREEN}""
