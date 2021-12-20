26e56d0b6c2749c72a56e40d173b5ae78a18f190
Awnrt <74102659+Awnrt@users.noreply.github.com>
2021-12-20 13:22:29 +0000

    Update setup_kde.sh

--- setup_kde.sh
+++ setup_kde.sh
@@ -15,8 +15,6 @@ pacman -S plasma sddm --noconfirm
 systemctl enable sddm.service
 pacman -S konsole --noconfirm
 
-printf ${LIGHTRED}"\nFinished!\nNow you need to add this stuff into modules list PATH /etc/mkinitcpio.conf\nMODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)\nAnd after that you can EXIT and reboot your system\n"
-printf ${LIGHTGREEN}""
 cd /mnt
 cd ..
 rm post_chroot.sh
